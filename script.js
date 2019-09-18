57b9812209970f4026d3a038f278d0677a8a4244
Nicklas Vilhelmsen <nich2290@stud.kea.dk>
2019-09-18 14:38:22 +0200

    sorting, filtering works. Popup works too, but with minor bugs

--- script.js
+++ script.js
@@ -1,161 +1,279 @@
-let indhold;
-let filter = "alle";
-let hus = [];
+"use strict";
 
-document.addEventListener("DOMContentLoaded", hentJson);
-//Henter JSON fil i asynkron function.
+window.addEventListener("DOMContentLoaded", start);
 
-// LOAD
-async function hentJson() {
-  console.log("Henter json");
-  // Fortæller os om async functionen bliver loaded
-  const url = "http://petlatkea.dk/2019/students1991.json";
+const allStudents = [];
+let currentList = [];
+let sortButton;
+let studentHouse;
+let Filter;
 
-  // Henter data filen
-  const myJson = await fetch(url);
-  // den hentede data skal tolkes som json
-  indhold = await myJson.json();
-  //Kalder funktion der viser data i DOM
+function start() {
+  console.log("ready");
 
-  visIndhold();
+  //   document.querySelector(".sortingnav").addEventListener("change", selectSorting);
+  // TODO: Add event-listeners for filter-buttons
 
-  //Leder async functionen videre til "visIndhold" functionen, hvor indholdet bliver hentet ind.
-}
-// Show Content
-function visIndhold() {
-  //Destinationen for hvor vi vil have indholdet hen
-  let dest = document.querySelector(".students");
-  //Template i destinationen, som danner opbygningen af det aktuelle post, i html.
-  let temp = document.querySelector("template");
+  document.querySelector(".students").addEventListener("click", clickSomething);
 
-  //En "For each" funktion, som gør at hvert nyt post bliver sat i hver sin template, altså bliver indholdet loopet. og overskriver dermed ikke hinanden
-  indhold.forEach(student => {
-    if (filter == "alle" || filter == student.house) {
-      //cloneNode skaber en kopi af "template".
-      let klon = temp.cloneNode(true).content;
-      console.log(student.fullname);
-      klon.querySelector("h3").innerHTML = student.fullname;
-      klon.querySelector(".house").innerHTML = student.house;
-
-      //Sender klonen retur til vores destination "student" og processen gentages.
-      dest.appendChild(klon);
-      dest.lastElementChild.addEventListener("click", () => {
-        visSingle(student);
-
-        function visSingle(student) {
-          //This function shows the selected student in a popup window and needs abit more work design wise. The functionality is there, but the UX needs tweaks.
-          document.querySelector("#indhold").innerHTML = `
-                    <article class="singlestudent">
-                        <h2>${student.fullname}</h2>
-                        <p>${student.house}.</p>
-                    </article>
-                       `;
-          document.querySelector("#popup").style.display = "block";
-          studentHouse = `${student.house}`;
-          popUpColors();
-          document
-            .querySelector("#popup #luk")
-            .addEventListener("click", close);
-        }
-
-        function close() {
-          document.querySelector("#popup").style.display = "none";
-        }
-      });
-    }
+  document.querySelectorAll(".sort").forEach(elm => {
+    elm.addEventListener("click", setSort);
   });
-}
-function popUpColors() {
-  //colors of houses
-  // This function is supposed to display different colors based on the house that is chosen, and is executed with an IF statement.
-  const Gryffindor = "#4c0405";
-  const gryf2 = "#f3c019";
-  const Hufflepuff = "#f3de07";
-  const huff2 = "#0c0d08";
-  const Slytherin = "#234723";
-  const slyth2 = "#9e9996";
-  const rave1 = "#725438";
-  const Ravenclaw = "#0b304a";
-  const teksten = "#ffffff";
-  if (studentHouse == "Gryffindor") {
-    document.querySelector("#popup").style.backgroundColor = Gryffindor;
-  } else if (studentHouse == "Hufflepuff") {
-    document.querySelector("#popup").style.backgroundColor = Hufflepuff;
+  document.querySelectorAll(".filter").forEach(elm => {
+    elm.addEventListener("click", setFilter);
+  });
 
-    document.querySelector("h2").style.color = teksten;
-    document.querySelector("p").style.color = teksten;
-  } else if (studentHouse == "Ravenclaw") {
-    document.querySelector("#popup").style.backgroundColor = Ravenclaw;
-  } else if (studentHouse == Slytherin) {
-    document.querySelector("#popup").style.backgroundColor = Slytherin;
+  loadJSON();
+}
+
+function clickSomething(event) {
+  let element = event.target;
+  const id = element.dataset.id;
+
+  let indexCur = findByIdCur(id);
+  let indexAll = findByIdAll(id);
+
+  if (element.dataset.action === "remove") {
+    console.log("Remove button clicked");
+    element.parentElement.remove();
+    currentList.splice(indexCur, 1);
+    allStudents.splice(indexAll, 1);
   }
-}
-clickSorting();
 
-document.querySelectorAll(".filter").forEach(elm => {
-  elm.addEventListener("click", clickFilter);
-});
-
-function clickSorting() {
-  // This function is used to sort the context by either First name, last name or house and it works in the console so far. I need to work further on it and figure how i implement it in live.
-  console.log("Click sorting");
-
-  document
-    .querySelector("#firstname")
-    .addEventListener("click", sortByFirstName);
-  document.querySelector("#lastname").addEventListener("click", sortByLastName);
-  document.querySelector("#house").addEventListener("click", sortByHouse);
+  console.table(currentList);
 }
 
-function sortByFirstName() {
-  // Sorting works in console, but needs implementation to function in live-preview. I'm unsure about this.
-  console.log("sort by first name");
-  indhold.sort((a, b) => {
-    return a.fullname.localeCompare(b.fullname);
+function findByIdCur(id) {
+  return currentList.findIndex(obj => obj.id == id);
+}
+function findByIdAll(id) {
+  return allStudents.findIndex(obj => obj.id == id);
+}
+
+// function selectSorting(event) {
+//   const sortBy = event.target.value;
+//   sortListBy(sortBy);
+//   displayList(currentList);
+// }
+
+function loadJSON() {
+  fetch("http://petlatkea.dk/2019/hogwartsdata/students.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      // when loaded, prepare objects
+      prepareObjects(jsonData);
+    });
+}
+
+// function prepareObjects(jsonData) {
+//   jsonData.forEach(jsonObject => {
+//     // Create new object with cleaned data
+//     const student = Object.create(Student);
+
+//     const texts = jsonObject.fullname.trim().split(" ");
+//     const house = jsonObject.house.trim();
+
+//     student.gender = jsonObject.gender;
+//     student.house = house.charAt(0).toUpperCase() + house.slice(1).toLowerCase();
+//     student.firstName = texts[0].charAt(0).toUpperCase() + texts[0].slice(1).toLowerCase();
+
+//     student.middleName = texts[1];
+//     student.lastName = texts[2];
+//     // if (texts[1] == undefined) {
+//     //   return false;
+//     // } else {
+//     //   student.middleName = texts[1].charAt(0).toUpperCase() + texts[1].slice(1).toLowerCase();
+//     // }
+
+//     // if (texts[2] == undefined) {
+//     //   return false;
+//     // } else {
+//     //   student.lastName = texts[2].charAt(0).toUpperCase() + texts[2].slice(1).toLowerCase();
+//     // }
+
+//     student.id = uuidv4();
+
+//     allStudents.push(student);
+//   });
+
+//   rebuildList();
+// }
+function prepareObjects(jsonData) {
+  jsonData.forEach(jsonObject => {
+    // Create new object with cleaned data
+    const student = Object.create(Student);
+
+    const fullName = jsonObject.fullname.trim().split(" ");
+    console.log(fullName);
+    const house = jsonObject.house.trim();
+
+    student.gender = jsonObject.gender;
+
+    student.house = house.charAt(0).toUpperCase() + house.slice(1).toLowerCase();
+
+    student.firstName = fullName[0].charAt(0).toUpperCase() + fullName[0].slice(1).toLowerCase();
+
+    if (fullName.length === 2) {
+      console.log("studerende har intet mellemnavn");
+      student.lastName = fullName[1].charAt(0).toUpperCase() + fullName[1].slice(1).toLowerCase();
+    } else if (fullName.length === 3) {
+      console.log("studerende har et mellemnavn");
+      student.middleName = fullName[1].charAt(0).toUpperCase() + fullName[1].slice(1).toLowerCase();
+      student.lastName = fullName[2].charAt(0).toUpperCase() + fullName[2].slice(1).toLowerCase();
+    }
+
+    student.id = uuidv4();
+
+    allStudents.push(student);
   });
 
-  console.log(indhold);
+  rebuildList();
+}
+//UUID generator from: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    let r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
-function sortByLastName() {
-  // Implementation of this needs to be worked on, it is currently not working at all.
-  //   console.log("sort by last name");
-  //   let fullName = indhold.fullname;
-  //   const searchTerm = " ";
-  //   let lastName = indhold.substring(
-  //     fullName.indexOf(searchTerm) + 1,
-  //     fullName.indexOf(searchTerm, fullName.indexOf(searchTerm) + 1) + 1
-  //   );
-  //   indhold.sort((a, b) => {
-  //     return a.lastName.localeCompare(b.lastName);
-  //   });
-  //   console.log(indhold);
+function rebuildList() {
+  filterListBy("all");
+  displayList(currentList);
 }
 
-function sortByHouse() {
-  // Sorting works in console, but needs implementation to function in live-preview. I'm unsure about this.
-  console.log("sort by house");
-  indhold.sort((a, b) => {
-    return a.house.localeCompare(b.house);
+function setSort() {
+  sortButton = this.getAttribute("data-sort");
+  document.querySelectorAll(".sort").forEach(elm => {
+    elm.classList.remove("valgt");
+  });
+  this.classList.add("valgt");
+  sortCurrentStudents(sortButton);
+}
+
+function sortCurrentStudents(sortButton) {
+  // TODO: Sort the list that is received, before displaying it
+  currentList.sort((a, b) => {
+    let comp;
+    if (sortButton == "first_name") {
+      comp = a.firstName.localeCompare(b.firstName);
+    } else if (sortButton == "last_name") {
+      comp = a.lastName.localeCompare(b.lastName);
+    } else if (sortButton == "house") {
+      comp = a.house.localeCompare(b.house);
+    }
+    return comp;
   });
 
-  console.log(indhold);
+  displayList(currentList);
 }
 
-function clickFilter() {
-  // This function filters the content bases on which house they are in.
-  console.log("filtrering");
-  filter = this.getAttribute("data-hus");
-  document.querySelector(".students").textContent = " ";
+// function sortListBy(prop) {
+//   currentList.sort((a, b) => (a[prop] > b[prop] ? 1 : -1)); // Don't copy this sorting function, it sucks ...
+// }
+
+function filterListBy(filterBy) {
+  currentList = allStudents.filter(student => {
+    return true; // right now, just don't filter anything
+  });
+}
+function setFilter() {
+  Filter = this.dataset.hus;
   document.querySelectorAll(".filter").forEach(elm => {
     elm.classList.remove("valgt");
   });
   this.classList.add("valgt");
-  visIndhold();
+  console.log(Filter);
+  currentList = filtering(Filter);
+  displayList(currentList);
 }
-//     function sortByFirstName (){
-//       console.log)"sort by first name"
-//   let newList = ["draco", "harry", "hermione", "ron"];
 
-//   //TODO: Fix this later, implement actual sorting
-//
+function filtering(house) {
+  let filterlist = allStudents.filter(filterHouse);
+  function filterHouse(student) {
+    if (student.house == house || house == "alle") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return filterlist;
+}
+
+function displayList(students) {
+  // clear the list
+  document.querySelector(".students").innerHTML = "";
+
+  // build a new list
+  students.forEach(displayStudent);
+}
+
+function displayStudent(student) {
+  // create clone
+  const clone = document.querySelector("template").content.cloneNode(true);
+
+  // set clone data
+  clone.querySelector(".names").innerHTML = `${student.firstName} ${student.middleName} ${student.lastName}`;
+  //   clone.querySelector(".studentname").innerHTML += student.middleName;
+  //   clone.querySelector(".studentname").innerHTML += student.lastName;
+  clone.querySelector(".house").innerHTML = student.house;
+  //   clone.querySelector("[data-field=type]").textContent = animal.type;
+  //   clone.querySelector("[data-field=age]").textContent = animal.age;
+
+  //store the index on the button
+  clone.querySelector("[data-action=remove]").dataset.id = student.id;
+
+  // append clone to list
+  document.querySelector(".students").appendChild(clone);
+
+  document.querySelector(".students").lastElementChild.addEventListener("click", () => {
+    visSingle(student);
+
+    function visSingle(student) {
+      document.querySelector("#indhold").innerHTML = `
+                <article class="singlestudent">
+                    <h2>${student.firstName} ${student.middleName} ${student.lastName}</h2>
+                    <p>${student.house}.</p>
+                </article>
+                   `;
+      document.querySelector("#popup").style.display = "block";
+      document.querySelector("#popup #luk").addEventListener("click", close);
+      stylePopop();
+    }
+
+    function close() {
+      document.querySelector("#popup").style.display = "none";
+    }
+
+    studentHouse = `${student.house}`;
+    stylePopop();
+  });
+
+  function stylePopop() {
+    const Gryffindor = "#4c0405";
+    const Hufflepuff = "#f3de07";
+    const Ravenclaw = "#0b304a";
+    const Slytherin = "#234723";
+
+    if (studentHouse == "Gryffindor") {
+      document.querySelector("#popup").style.backgroundColor = Gryffindor;
+    } else if (studentHouse == "Hufflepuff") {
+      document.querySelector("#popup").style.backgroundColor = Hufflepuff;
+    } else if (studentHouse == "Ravenclaw") {
+      document.querySelector("#popup").style.backgroundColor = Ravenclaw;
+    } else if (studentHouse == "Slytherin") {
+      document.querySelector("#popup").style.backgroundColor = Slytherin;
+    }
+  }
+}
+
+// Our prototype Student
+const Student = {
+  firstName: "-firstName-",
+  middleName: "",
+  lastName: "",
+  gender: "-gender-",
+  house: "-house-",
+  id: "-id"
+};
